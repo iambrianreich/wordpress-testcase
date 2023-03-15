cbc0fc7f370105ab75652dfc099ff3001848664c
Reich Web Consulting <breich2617@gmail.com>
2023-03-14 21:15:25 -0400

    feat: base plugin file
    This isn't required for the project but is a required scaffold for conducting unit tests against a running WordPress.

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file contains BR\WordPress\TestCase\Plugin.
+ *
+ * @author Brian Reich <brian@brianreich.dev>
+ * @copyright MIT
+ */
+
+declare(strict_types=1);
+
+namespace BR\WordPress\TestCase;
+
+class Plugin
+{
+}
